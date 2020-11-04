f5293d99dad62c04c6a48689fbb21713d6966f56
Eugene_Zaukin <Eugene_Zaukin@mail.ru>
2020-11-04 16:51:22 +0700

    Lesson_5

--- src/Lesson_5/Car.java
+++ src/Lesson_5/Car.java
@@ -1,21 +1,34 @@
 package Lesson_5;
 
+import java.security.cert.CertificateNotYetValidException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car implements Runnable{
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+    private static CyclicBarrier cb;
+    private static Lock lock = new ReentrantLock();
+    private static CountDownLatch win = new CountDownLatch(2);
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier cb1) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb1;
     }
     @Override
     public void run() {
@@ -23,11 +36,22 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cb.await();
+            cb.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            synchronized (lock) {
+                win.countDown();
+                if (win.getCount() == 1) {
+                    System.out.println(this.name + " Победил!");
+                }
+            }
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
