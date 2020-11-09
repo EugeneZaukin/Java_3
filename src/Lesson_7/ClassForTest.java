daef019a3f0a3c18dfac92ca5e981a759a4e6c85
Eugene_Zaukin <Eugene_Zaukin@mail.ru>
2020-11-09 13:37:37 +0700

    Lesson_7

--- src/Lesson_7/ClassForTest.java
+++ src/Lesson_7/ClassForTest.java
@@ -2,4 +2,42 @@ package Lesson_7;
 
 public class ClassForTest {
 
+    @BeforeSuite
+    public static void before() {
+        System.out.println("Начало тестов");
+    }
+
+    @Test(priority = 2)
+    public static void test1() {
+        System.out.println("Тест 1");
+    }
+
+    @Test(priority = 4)
+    public static void test2() {
+        System.out.println("Тест 2");
+    }
+
+    @Test(priority = 5)
+    public static void test3() {
+        System.out.println("Тест 3");
+    }
+
+    @Test(priority = 3)
+    public static void test4() {
+        System.out.println("Тест 4");
+    }
+
+    @Test(priority = 1)
+    public static void test5() {
+        System.out.println("Тест 5");
+    }
+
+    @AfterSuite
+    public static void after() {
+        System.out.println("Конец тестов");
+    }
+//    @AfterSuite
+//    public static void after1() {
+//        System.out.println("Конец тестов");
+//    }
 }
--- src/Lesson_7/Main.java
+++ src/Lesson_7/Main.java
@@ -1,4 +1,59 @@
 package Lesson_7;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Class cl = ClassForTest.class;
+        Method[] methods = cl.getDeclaredMethods();
+        ArrayList<Method> arr = new ArrayList();
+        int countBefore = 0;
+        int countAfter = 0;
+
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                countBefore++;
+                if (countBefore > 1) {
+                    throw new RuntimeException("Может быть только один метод с аннотацией BeforeSuite");
+                }
+            }
+
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                countAfter++;
+                if (countAfter > 1) {
+                    throw new RuntimeException("Может быть только один метод с аннотацией AfterSuite");
+                }
+            }
+
+            if (m.isAnnotationPresent(Test.class)) {
+                arr.add(m);
+            }
+
+            arr.sort(new Comparator<Method>() {
+                @Override
+                public int compare(Method o1, Method o2) {
+                    return o1.getAnnotation(Test.class).priority() - o2.getAnnotation(Test.class).priority();
+                }
+            });
+        }
+
+        for (Method before : methods) {
+            if (before.isAnnotationPresent(BeforeSuite.class))
+            before.invoke(null);
+        }
+
+        for (Method tests : arr) {
+            tests.invoke(null);
+        }
+
+        for (Method after : methods) {
+            if (after.isAnnotationPresent(AfterSuite.class))
+            after.invoke(null);
+        }
+
+    }
 }
